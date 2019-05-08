eda30dd7920cd3b478766658700d24df6e06df07
R3mig <Remix@amorki.pl>
2019-05-08 13:32:06 +0200

    Camera implemented
    works

--- app/src/main/java/com/example/moodle_cam/MainActivity.java
+++ app/src/main/java/com/example/moodle_cam/MainActivity.java
@@ -2,8 +2,15 @@ package com.example.moodle_cam;
 
 
 import android.app.Activity;
+import android.content.ContentResolver;
 import android.content.Intent;
+import android.graphics.Bitmap;
+import android.media.Image;
+import android.net.Uri;
+import android.os.Environment;
+import android.provider.MediaStore;
 import android.support.design.widget.FloatingActionButton;
+import android.support.v4.content.FileProvider;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
@@ -27,16 +34,24 @@ import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.io.OutputStream;
 import java.nio.charset.Charset;
+import java.text.SimpleDateFormat;
 import java.util.ArrayList;
+import java.util.Date;
 import java.util.List;
 
+
 public class MainActivity extends AppCompatActivity {
-    private static final int READ_REQUEST_CODE = 42;
+    private static final int REQUEST_IMAGE_CAPTURE = 1;
+    static final int REQUEST_TAKE_PHOTO = 1;
+    private static final int READ_REQUEST_CODE = 2;
+    private static final int TAKE_PICTURE = 3;
+    private Uri imageUri;
 
 
 
     private List<Student> theStudent = new ArrayList<Student>();
     private String stringUri;
+    private String currentPhotoPath;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -60,14 +75,11 @@ public class MainActivity extends AppCompatActivity {
                 Student ClickedStudent = theStudent.get(position);
                 String message = "Gewählt wurde Schüler: " + ClickedStudent.getName();
                 Toast.makeText(MainActivity.this, message, Toast.LENGTH_SHORT).show(); //Toast ist ein Popup von Unten... ;D
-                startCamera();
+
+                dispatchTakePictureIntent();
+                galleryAddPic();
 
             }
-
-            private void startCamera() {
-                // TODO
-            }
-
         }); //ja, das muss so.
 
         // Zip -Button
@@ -160,7 +172,7 @@ public class MainActivity extends AppCompatActivity {
             //find the Student to work with
             Student currentStudent = theStudent.get(position);
 
-            //fill the Preview  NOTE: Add in camerawhen pictures are possible
+            //fill the Preview  NOTE: Add in camera when pictures are possible
             //ImageView imageView = (ImageView) itemView.findViewById(R.id.previewpicture);
             //imageView.setImageResource(Integer.parseInt(currentStudent.getIconID()));
 
@@ -194,40 +206,99 @@ public class MainActivity extends AppCompatActivity {
 
 
     }
+// ++++++++++++++++++++++++++++++++++++++++++[ Camera ]++++++++++++++++++++++++++++
+
+    private void dispatchTakePictureIntent() {
+        Intent takePictureIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+        // Ensure that there's a camera activity to handle the intent
+        if (takePictureIntent.resolveActivity(getPackageManager()) != null) {
+            // Create the File where the photo should go
+            File photoFile = null;
+            try {
+                photoFile = createImageFile();
+            } catch (IOException ex) {
+                Log.wtf("MainActivity","Error occurred while creating the File");
+            }
+            // Continue only if the File was successfully created
+            if (photoFile != null) {
+                Uri photoURI = FileProvider.getUriForFile(this,
+                        "com.example.moodle_cam.fileprovider",
+                        photoFile);
+                takePictureIntent.putExtra(MediaStore.EXTRA_OUTPUT, photoURI);
+                startActivityForResult(takePictureIntent, REQUEST_TAKE_PHOTO);
+
+            }
+        }
+    }
+    private File createImageFile() throws IOException {
+        // Create an image file name
+        String timeStamp = new SimpleDateFormat("yyyyMMdd_HHmmss").format(new Date());
+        String studentID = "87231";
+        String imageFileName = studentID+timeStamp;
+        File storageDir = getExternalFilesDir(Environment.DIRECTORY_PICTURES);
+        File image = File.createTempFile(
+                imageFileName,  /* prefix */
+                ".jpg",         /* suffix */
+                storageDir      /* directory */
+        );
+
+        // Save a file: path for use with ACTION_VIEW intents
+        currentPhotoPath = image.getAbsolutePath();
+        Log.w("createImageFile","Path: " +currentPhotoPath);
+
+        return image;
+    }
+
+    private void galleryAddPic() {
+        Intent mediaScanIntent = new Intent(Intent.ACTION_MEDIA_SCANNER_SCAN_FILE);
+        File f = new File(currentPhotoPath);
+        Uri contentUri = Uri.fromFile(f);
+        mediaScanIntent.setData(contentUri);
+        this.sendBroadcast(mediaScanIntent);
+    }
+
+
 
     @Override
     public void onActivityResult(int requestCode, int resultCode,
                                  Intent resultData) {
+        super.onActivityResult(requestCode, resultCode, resultData);
+        switch(requestCode){
+            case 2: //open the File-Browser
 
-        if (requestCode == READ_REQUEST_CODE && resultCode == Activity.RESULT_OK)
-        {
+                if (requestCode == READ_REQUEST_CODE && resultCode == Activity.RESULT_OK) {
 
-            if (resultData != null) {
-                try {
-                    String destination = getFilesDir().getPath();
-                    InputStream src = getContentResolver().openInputStream(resultData.getData()); // use the uri to create an inputStream
+                    if (resultData != null) {
+                        try {
+                              String destination = getFilesDir().getPath();
+                              InputStream src = getContentResolver().openInputStream(resultData.getData()); // use the uri to create an inputStream
 
-                    try {
+                             try {
 
-                        convertInputStreamToFile(src, destination);
+                                      convertInputStreamToFile(src, destination);
 
-                        //get the values from the csv for the list Objects after copy
-                        readStudentData();
-                        populateListView();
+                                        //get the values from the csv for the list Objects after copy
+                                         readStudentData();
+                                          populateListView();
 
 
-                    } catch (IOException e) {
-                        e.printStackTrace();
-                        System.out.print("error in upload");
-                    }
-                } catch (FileNotFoundException ex) {
-                    }
-                String destination = getFilesDir().getPath();
-                Toast.makeText(MainActivity.this, "Success!: CSV-File copyed to : " +destination  , Toast.LENGTH_SHORT).show();
+                             } catch (IOException e)    {
+                                      e.printStackTrace();
+                             }
+                        } catch (FileNotFoundException ex) {
+                        }
+                             String destination = getFilesDir().getPath();
+                            Toast.makeText(MainActivity.this, "Success!: CSV-File copyed to : " +destination  , Toast.LENGTH_SHORT).show();
+                     }
+                 }
+                break;//first case
 
-            }
+            case 1:
+                if (requestCode == REQUEST_TAKE_PHOTO && resultCode == RESULT_OK) {
+
+                    galleryAddPic();
+                }
         }
-
 
     }
     public static void convertInputStreamToFile(InputStream is, String destination) throws IOException
